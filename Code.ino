c9cb35de87bce935f7b85d9e56bf34ae426d8f72
Umer Abid <jeremymathieu65@gmail.com>
2022-05-16 02:23:32 +0500

    Added code to store DHT sensor readings

--- Code.ino
+++ Code.ino
@@ -1,7 +1,16 @@
 #include <WiFi.h>;
+#include <DHT.h>
+
+#define DHT_TYPE DHT11
+#define DHT_Pin 12
 
 const char* ssid = "your ssid";
 const char* password = "your password";
+
+DHT dht(DHT_Pin, DHT_TYPE)
+
+float temperature;
+float humidity;
 
 void setup() {
   // put your setup code here, to run once:
@@ -18,6 +27,9 @@ void setup() {
   Serial.print("IP Address: ");
   Serial.println(WiFi.localIP());
 
+  pinMode(DHT_Pin, INPUT);
+  dht.begin();
+
 }
 
 void loop() {
@@ -26,9 +38,15 @@ void loop() {
     Serial.println("Connected");
     Serial.print("IP Address: ");
     Serial.println(WiFi.localIP());
+    get_DHT_readngs();
     delay(5000);
   }
   else {
     Serial.println("Connection Lost");
   }
+}
+
+void get_DHT_readings() {
+  temperature = dht.readTemperature();
+  humidity = dht.readHumidity();
 }
