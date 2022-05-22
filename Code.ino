19dc601ed03ff2988bb0e08779bdabc0617dd335
Umer Abid <jeremymathieu65@gmail.com>
2022-05-22 17:59:38 +0500

    Connected arduino to front-end and added live graphs

--- Code.ino
+++ Code.ino
@@ -1,5 +1,8 @@
 #include <WiFi.h>;
-#include <DHT.h>
+#include <DHT.h>;
+#include "ESPAsyncWebServer.h";
+#include "AsyncTCP.h"
+#include "SPIFFS.h"
 
 #define DHT_TYPE DHT11
 #define DHT_Pin 4
@@ -9,12 +12,42 @@
 const char* ssid = "Redmi 9C";
 const char* password = "a629fg65";
 
+AsyncWebServer server(80);
+
 DHT dht(DHT_Pin, DHT_TYPE);
 
 float temperature;
 float humidity;
 float gas_concentration;
 float light_intensity;
+
+String get_temperature() {
+  temperature = dht.readTemperature();
+  Serial.print("Temperature: ");
+  Serial.println(temperature);
+  return String(temperature);
+}
+
+String get_humidity() {
+  humidity = dht.readHumidity();
+  Serial.print("Humidity");
+  Serial.println(humidity);
+  return String(humidity);
+}
+
+String get_gas_readings() {
+  gas_concentration = analogRead(MQ5_Pin);
+  Serial.print("Gas Level: ");
+  Serial.println(gas_concentration);
+  return String(gas_concentration);
+}
+
+String get_light_intensity() {
+  light_intensity = analogRead(LDR_Pin);
+  Serial.print("Light Intensity: ");
+  Serial.println(light_intensity);
+  return String(light_intensity);
+}
 
 void setup() {
   // put your setup code here, to run once:
@@ -27,53 +60,37 @@ void setup() {
     delay(500);
   }
 
+  if(!SPIFFS.begin()){
+    Serial.println("An Error has occurred while mounting SPIFFS");
+    return;
+  }
+
   Serial.println("\nConnected to WiFi network");
   Serial.print("IP Address: ");
   Serial.println(WiFi.localIP());
 
   pinMode(DHT_Pin, INPUT);
   dht.begin();
-
   pinMode(MQ5_Pin, INPUT);
-
   pinMode(LDR_Pin, INPUT);
 
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send(SPIFFS, "/index.html");
+  });
+  server.on("/temperature", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send_P(200, "text/plain", get_temperature().c_str());
+  });
+  server.on("/humidity", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send_P(200, "text/plain", get_humidity().c_str());
+  });
+  server.on("/gas-concentration", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send_P(200, "text/plain", get_gas_readings().c_str());
+  });
+  server.on("/light-intensity", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send_P(200, "text/plain", get_light_intensity().c_str());
+  });
+
+  server.begin();
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-  if (WiFi.status() == WL_CONNECTED) {
-    Serial.println("Connected");
-    Serial.print("IP Address: ");
-    Serial.println(WiFi.localIP());
-    get_DHT_readings();
-    get_MQ5_readings();
-    get_LDR_readings();
-    delay(5000);
-  }
-  else {
-    Serial.println("Connection Lost");
-  }
-}
-
-void get_DHT_readings() {
-  temperature = dht.readTemperature();
-  
-  humidity = dht.readHumidity();
-  Serial.print("Temperature: ");
-  Serial.println(temperature);
-  Serial.print("Humidity: ");
-  Serial.println(humidity);
-}
-
-void get_MQ5_readings() {
-  gas_concentration = analogRead(MQ5_Pin);
-  Serial.print("Gas Level: ");
-  Serial.println(gas_concentration);
-}
-
-void get_LDR_readings() {
-  light_intensity = analogRead(LDR_Pin);
-  Serial.print("Light Intensity: ");
-  Serial.println(light_intensity);
-}
+void loop() {}
